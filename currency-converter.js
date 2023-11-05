3df35b28209ab291613cd553bb3f2abfb7343a9c
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-11-05 19:50:04 +0900

    async await

--- currency-converter.js
+++ currency-converter.js
@@ -1,17 +1,15 @@
 const axios = require("axios");
 
-const getExchangeRate = (fromCurrency, toCurrency) => {
-  axios
-    .get(
-      "http://data.fixer.io/api/latest?access_key=1edd5466135e2c9bc8c3026a21bb5fbe"
-    )
-    .then((response) => {
-      const rate = response.data.rates;
-      const euro = 1 / rate[fromCurrency];
-      const exchangeRate = euro * rate[toCurrency];
+const getExchangeRate = async (fromCurrency, toCurrency) => {
+  const response = await axios.get(
+    "http://data.fixer.io/api/latest?access_key=1edd5466135e2c9bc8c3026a21bb5fbe"
+  );
 
-      console.log(exchangeRate);
-    });
+  const rate = response.data.rates;
+  const euro = 1 / rate[fromCurrency];
+  const exchangeRate = euro * rate[toCurrency];
+
+  console.log(exchangeRate);
 };
 
 getExchangeRate("USD", "EUR");
